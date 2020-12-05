b9073d228a213108c2b4b724879bc42ddc3060ce
wonderxue <1522038223@qq.com>
2020-12-05 09:45:12 +0000

    this is for mpu

---
+++ build/src/CMakeFiles/mpu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpu.dir/GY_521.c.o"
+  "CMakeFiles/mpu.dir/main.c.o"
+  "../../bin/mpu.pdb"
+  "../../bin/mpu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
